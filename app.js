f78e43702ea95c53854c471c1e94b4eb1246ea40
unknown <meghanilva@live.in>
2020-11-14 19:49:35 +0530

    chal_10 Done

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
 
 app.get("/",function(req,res  ){
  res.render('home',{hsContent: homeStartingContent},);
@@ -34,6 +38,14 @@ app.get("/compose",function(req,res){
   res.render('compose');
 })
 
+app.post("/compose", function(req,res ){
+  console.log(req.body);
+ const post={
+   title: req.body.postTitle,
+   content:req.body.postBody
+ };
+ console.log(post );
+} );
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
